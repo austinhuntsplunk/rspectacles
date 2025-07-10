bd3415a654139500bb7e4a0ee476747aebd51002
Austin Hunt <austinhunt@splunk.com>
2025-07-10 12:56:41 -0400

    start_with and end_with tests on arrays

--- start_with_and_end_with_matchers_spec.rb
+++ start_with_and_end_with_matchers_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe 'start_with and end_with matchers' do
     describe 'hot chocolate' do
       it { is_expected.to start_with('hot') }
     end 
+
+    describe [:a, :b, :c, :d] do 
+      it { is_expected.to start_with(:a) } 
+      it { is_expected.to start_with(:a, :b) }
+    end
   end
 
   describe 'end_with matcher' do
@@ -17,5 +22,12 @@ RSpec.describe 'start_with and end_with matchers' do
     describe 'hot chocolate' do
       it { is_expected.to end_with('chocolate') }
     end 
+
+    describe [:a, :b, :c, :d] do 
+      it { is_expected.to end_with(:d) } 
+      it { is_expected.to end_with(:c, :d) }
+    end
   end
+
+  describe 
 end
